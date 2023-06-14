493f51172e18fc8fd7abf88bd077c61d3f52784c
wangao-ubuntu <17358077637@163.com>
2023-06-14 09:29:25 +0800

    modify some bugs(plt.imsave)

--- shRegression/test.py
+++ shRegression/test.py
@@ -9,6 +9,7 @@ from torch.utils.tensorboard import SummaryWriter
 import models_vit
 import cv2
 from PIL import Image
+import matplotlib.pyplot as plt
 
 def read_hdr(hdr_path):
     hdr_img = cv2.imread(hdr_path, flags=cv2.IMREAD_UNCHANGED | cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
@@ -58,8 +59,8 @@ model = models_vit.__dict__['vit_base_patch16'](
         global_pool=False,
     )
 
-model.load_state_dict(torch.load("./runs/TimmVIT_shiamge 0.0442 0.0041/model-100.pth"),strict=False)
-print(model.load_state_dict(torch.load("./runs/TimmVIT_shiamge 0.0442 0.0041/model-100.pth"),strict=False))
+model.load_state_dict(torch.load("./runs/256x256InputVit/weights/model-73.pth"),strict=False)
+print(model.load_state_dict(torch.load("./runs/256x256InputVit/weights/model-73.pth"),strict=False))
 model.to(device)
 
 input_path =  '/media/wangao/DataDisk/Study/Dataset/Laval Indoor HDR Daset/train/HDRInput/'
@@ -67,18 +68,19 @@ input_names = [f for f in os.listdir(input_path) if f.endswith('.exr')]
 for name in input_names:
     hdr_path = input_path + name
     hdr = read_hdr(hdr_path)
-    image = (torch.from_numpy(tone(hdr))).permute(2, 0, 1).to(device)
+    image = (torch.from_numpy(tone(hdr))).permute(2, 0, 1).unsqueeze(0).to(device)
     predict = model(image)
     gt_path = '/media/wangao/DataDisk/Study/Dataset/warped_Laval_Indoor_HDR_Dataset/SHGT/train/' + name.replace('.exr','.txt')
-    gt = torch.FloatTensor(np.loadtxt(gt_path))
+    gt = torch.FloatTensor(np.loadtxt(gt_path)).unsqueeze(0).to(device)
     shimagepred = utils.reconstruction(coeff, predict, 'pred')
     shimagegt = utils.reconstruction(coeff, gt, 'gt')
     shimagepred = shimagepred.detach().cpu().numpy()
     shimagegt = shimagegt.detach().cpu().numpy()
-    result = np.vstack((shimagegt,shimagepred))
-    result = Image.fromarray(result.astype('uint8'))
-    result.save(sv_dir+'result' + '/{}'.format(name.replace('.exr','.jpg')))
+    result = np.hstack((shimagegt,shimagepred))
+    plt.imsave(sv_dir+'result' + '/{}'.format(name.replace('.exr','.jpg')),tone(result[0]))
+    # result = Image.fromarray(result[0])
+    # result.save(sv_dir+'result' + '/{}'.format(name.replace('.exr','.jpg')))
     parametric_lights = {'sh_image': shimagepred}
     with open((sv_dir + os.path.basename(hdr_path).replace('exr', 'pickle')), 'wb') as handle:
         pickle.dump(parametric_lights, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
+    print(sv_dir+'result' + '/{}'.format(name.replace('.exr','.jpg')))
